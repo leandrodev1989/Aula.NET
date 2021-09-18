3b5a027d9e9a4fb12d5fc95ac4b1e2f6b7ae8b99
leandrodev1989 <grautec@live.com>
2021-09-18 18:28:32 -0300

    Update Program.cs

--- 03-Classes/Program.cs
+++ 03-Classes/Program.cs
@@ -6,7 +6,7 @@ namespace Classes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Deus é Fiel!");
         }
     }
 }
